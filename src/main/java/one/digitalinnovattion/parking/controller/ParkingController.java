9907bcd5b4cfcca57d94463f6b0e8ef635e0183c
EdisonLemesSantos <edisonlemessantos@gmail.com>
2022-11-19 10:49:54 -0300

    Primeiro edpoint de lista

---
+++ src/main/java/one/digitalinnovattion/parking/controller/ParkingController.java
@@ -0,0 +1,33 @@
+package one.digitalinnovattion.parking.controller;
+
+
+import one.digitalinnovattion.parking.controller.dto.ParkingDTO;
+import one.digitalinnovattion.parking.controller.mapper.ParkingMapper;
+import one.digitalinnovattion.parking.model.Parking;
+import one.digitalinnovattion.parking.service.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public List<ParkingDTO> fidAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+
+    }
+}
